4eb9fd56e175f49031e3330e19bcd4800c3d5038
Tokiniaina ANDRIAMAHEFASOA <tokiniaina.andriamahefasoa@linkeo.com>
2025-03-18 18:57:52 +0300

    Désactivation de test liquibase

--- tasks-service/src/test/java/com/kinga/tasksservice/service/EventServiceTest.java
+++ tasks-service/src/test/java/com/kinga/tasksservice/service/EventServiceTest.java
@@ -12,12 +12,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest(classes = Application.class)
-@ExtendWith(SpringExtension.class)
-@TestPropertySource(properties = "spring.liquibase.enabled=false")
 class EventServiceTest {
 
 }
