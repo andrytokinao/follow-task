59bd710918b91ca5fb3eef2c503653a7da654097
Tokiniaina ANDRIAMAHEFASOA <tokiniaina.andriamahefasoa@linkeo.com>
2025-09-08 11:46:49 +0300

    Autoriser tous pour le cors origine

--- tasks-service/src/main/java/com/kinga/followtask/config/CorsConfig.java
+++ tasks-service/src/main/java/com/kinga/followtask/config/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200","http://localhost:8083"));
+        corsConfiguration.setAllowedOriginPatterns(Collections.singletonList("*"));
         corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE","OPTIONS"));
         corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
         corsConfiguration.setAllowCredentials(true);
