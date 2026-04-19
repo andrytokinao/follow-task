a58f701adbdbccef816c7e3564c44e9223ec43de
Andry Tokiniaina <tokinao.mg@gmail.com>
2026-04-19 09:36:16 +0300

    fixe config

--- tasks-service/src/main/java/com/kinga/followtask/config/LiquibaseConfig.java
+++ tasks-service/src/main/java/com/kinga/followtask/config/LiquibaseConfig.java
@@ -2,14 +2,16 @@ package com.kinga.followtask.config;
 
 import liquibase.Liquibase;
 import liquibase.database.Database;
-import liquibase.database.DatabaseConnection;
-import liquibase.database.core.MySQLDatabase;
+import liquibase.database.DatabaseFactory;
 import liquibase.database.jvm.JdbcConnection;
 import liquibase.exception.LiquibaseException;
 import liquibase.resource.ClassLoaderResourceAccessor;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -17,16 +19,47 @@ import java.sql.SQLException;
 
 @Configuration
 public class LiquibaseConfig {
-    @Bean
-    @Qualifier("liquibaseMaster")
-    public Liquibase liquibaseMaster(@Qualifier("masterDbDataSource") DataSource masterDbDataSource) throws LiquibaseException, SQLException {
+
+    private static final Logger log = LoggerFactory.getLogger(LiquibaseConfig.class);
+
+    @Value("${spring.liquibase.enabled:true}")
+    private boolean liquibaseEnabled;
+
+    @Value("${spring.liquibase.change-log:db-changelog/db.changelog-master.xml}")
+    private String changeLog;
+
+    @Bean("liquibaseMaster")
+    public Liquibase liquibaseMaster(
+            @Qualifier("masterDbDataSource") DataSource masterDbDataSource)
+            throws LiquibaseException, SQLException {
+
+        if (!liquibaseEnabled) {
+            log.info("Liquibase est désactivé (spring.liquibase.enabled=false)");
+            return null;
+        }
+
+        String cleanPath = changeLog.replaceFirst("^classpath\\*?:", "");
+        log.info("Liquibase démarrage avec changelog : {}", cleanPath);
+
         Connection connection = masterDbDataSource.getConnection();
         JdbcConnection jdbcConnection = new JdbcConnection(connection);
-        Database database = new MySQLDatabase();
-        database.setConnection(jdbcConnection);
-        Liquibase liquibase = new Liquibase("db/changelog/db.changelog-master.xml", new ClassLoaderResourceAccessor(), database);
+
+        // Détection automatique du type de base (H2, MySQL, PostgreSQL...)
+        Database database = DatabaseFactory.getInstance()
+                .findCorrectDatabaseImplementation(jdbcConnection);
+
+        log.info("Liquibase connecté sur : {} ({})",
+                database.getDatabaseProductName(),
+                database.getConnection().getURL());
+
+        Liquibase liquibase = new Liquibase(
+                cleanPath,
+                new ClassLoaderResourceAccessor(getClass().getClassLoader()),
+                database
+        );
+
         liquibase.update("");
+        log.info("Liquibase migrations exécutées avec succès");
         return liquibase;
     }
-
 }
