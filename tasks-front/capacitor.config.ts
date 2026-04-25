f89aabe2b0dc42c0a37ca735e6e39928f0d66224
Andry Tokiniaina <tokinao.mg@gmail.com>
2026-04-25 19:05:33 +0300

    capacitor config

---
+++ tasks-front/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.kinga.paikady',
+  appName: 'Paikady',
+  webDir: '../tasks-service/src/main/resources/static'
+};
+
+export default config;
