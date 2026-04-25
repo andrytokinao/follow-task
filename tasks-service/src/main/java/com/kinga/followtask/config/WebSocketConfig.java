567de0c2d9f7f33376eb051ede18dad20a09d155
Andry Tokiniaina <tokinao.mg@gmail.com>
2026-04-25 09:45:08 +0300

    cross origine WebSocketConfig http://localhost:4200

--- tasks-service/src/main/java/com/kinga/followtask/config/WebSocketConfig.java
+++ tasks-service/src/main/java/com/kinga/followtask/config/WebSocketConfig.java
@@ -1,5 +1,3 @@
-
-
 package com.kinga.followtask.config;
 
 import org.slf4j.Logger;
@@ -7,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -25,14 +22,16 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(final StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws");
-        registry.addEndpoint("/ws").withSockJS().setWebSocketEnabled(true);
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins("http://localhost:4200");
 
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins("http://localhost:4200")
+                .withSockJS()
+                .setWebSocketEnabled(true);
     }
+
     @Override
-    public void configureClientInboundChannel(ChannelRegistration registration){
+    public void configureClientInboundChannel(ChannelRegistration registration) {
     }
-
 }
-
-
