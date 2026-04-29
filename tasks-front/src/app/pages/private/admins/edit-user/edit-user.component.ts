745a8f6b05a347d79f63894ef6124738a51c72db
Andry Tokiniaina <tokinao.mg@gmail.com>
2026-04-29 11:42:34 +0300

    Amelioration de formulaire create , edit user

--- tasks-front/src/app/pages/private/admins/edit-user/edit-user.component.ts
+++ tasks-front/src/app/pages/private/admins/edit-user/edit-user.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import {UserService} from "../../../../services/user.service";
-import {MemberGroupe, User} from "../../../../type/issue";
-import {environment} from "../../../../../environments/environment";
-import {supprimerTypename} from "../../../../type/graphql.operations";
+import { UserService } from "../../../../services/user.service";
+import { MemberGroupe, User } from "../../../../type/issue";
+import { environment } from "../../../../../environments/environment";
+import { supprimerTypename } from "../../../../type/graphql.operations";
 
 @Component({
   standalone: false,
@@ -12,55 +12,60 @@ import {supprimerTypename} from "../../../../type/graphql.operations";
 })
 export class EditUserComponent {
 
-  constructor(private userService:UserService) {
-  }
-  savingStatus:string ='';
-  tempPhoto:string | ArrayBuffer | null = null;
-  editingName: boolean = true;
-  user : User | any ={} ;
+  constructor(private userService: UserService) {}
+
+  savingStatus: string = '';
+  tempPhoto: string | ArrayBuffer | null = null;
+  user: User | any = {};
   activeModal: any;
-  action: string ="";
-  memberGroupes:MemberGroupe[] =[];
+  action: string = "";
+  memberGroupes: MemberGroupe[] = [];
   selectedPhoto: File | any = {};
   isCreate: boolean = false;
-  editName() {
 
-  }
-  photoUrl(){
-    if(this.tempPhoto)
-      return this.tempPhoto;
-    if (this.user && this.user.photo) {
-      return  environment.apiURL+'photo/'+this.user.photo;
-    } else {
-      return 'assets/photo.png';
+  photoUrl() {
+    if (this.tempPhoto) return this.tempPhoto;
+    if (this.user?.photo) {
+      return environment.apiURL + 'photo/' + this.user.photo;
     }
+    return 'assets/photo.png';
   }
-  loadGroupeMember(){
+
+  loadGroupeMember() {
     this.userService.loadGroupeMember(this.user.id).subscribe(
-      (res:any)=>{
+      (res: any) => {
         this.memberGroupes = supprimerTypename(res.data.loadGroupeMember);
-      },(err)=>{
-        console.error("loadGroupeMember" +err);
+      },
+      (err) => {
+        console.error("loadGroupeMember: " + err);
       }
-    )
+    );
   }
 
   saveUser() {
-    this.userService.saveUser(this.user).subscribe(res => {
-      this.savingStatus = 'success';
-    }, error => {
-      this.savingStatus = 'error';
-      console.error("saveUser ==> " + error);
-    })
+    this.userService.saveUser(this.user).subscribe(
+      () => {
+        this.savingStatus = 'success';
+        // Fermer le modal après un court délai pour que l'utilisateur voie le message
+        setTimeout(() => {
+          this.activeModal.close('saved');
+        }, 800);
+      },
+      (error) => {
+        this.savingStatus = 'error';
+        console.error("saveUser ==> " + error);
+      }
+    );
   }
 
   selectPhoto($event: any) {
-    const file: File = $event!.target.files[0];
+    const file: File = $event?.target.files[0];
     if (file) {
       this.selectedPhoto = file;
       this.previewImage(file);
     }
   }
+
   previewImage(file: File): void {
     const reader = new FileReader();
     reader.readAsDataURL(file);
@@ -68,19 +73,25 @@ export class EditUserComponent {
       this.tempPhoto = reader.result;
     };
   }
+
   openPhotoInput(): void {
     document.getElementById('photoInput')?.click();
   }
+
   uploadPhoto() {
-    this.userService.upload(this.selectedPhoto,this.user.id).subscribe(res=>{
+    this.userService.upload(this.selectedPhoto, this.user.id).subscribe(
+      () => {
+        this.tempPhoto = null; // Cacher le bouton après upload
       },
-      error => {
+      (error) => {
+        console.error("uploadPhoto ==> " + error);
       }
-    )
+    );
   }
-  loadUser(userId:String){
-    this.userService.getUser(userId).subscribe((user:User)=>{
+
+  loadUser(userId: string) {
+    this.userService.getUser(userId).subscribe((user: User) => {
       this.user = user;
-    })
+    });
   }
 }
