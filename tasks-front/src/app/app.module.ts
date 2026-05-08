75c49ce60256dc866851baf73617f15b3f5c952b
Andry Tokiniaina <tokinao.mg@gmail.com>
2026-05-08 06:04:15 +0300

    Fixe import

--- tasks-front/src/app/app.module.ts
+++ tasks-front/src/app/app.module.ts
@@ -1,85 +1,78 @@
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule, isDevMode} from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule, isDevMode } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {appRoutes, AppRoutingModule} from "./app.routing.module";
-import {AppComponent} from "./app.component";
-import {BrowserAnimationsModule, provideAnimations} from '@angular/platform-browser/animations';
-import { provideNativeDateAdapter} from '@angular/material/core';
-import {FormsModule, } from '@angular/forms';
-import {HTTP_INTERCEPTORS, HttpClientModule,} from '@angular/common/http';
+import { appRoutes, AppRoutingModule } from "./app.routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { FormsModule } from '@angular/forms';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { provideRouter } from "@angular/router";
 
-import {CookieService} from "ngx-cookie-service";
-import {HttpInterceptorService} from "./services/http.service";
-import {provideToastr, ToastrModule} from "ngx-toastr";
-import {MarkdownModule} from "ngx-markdown";
-import {QuillModule} from "ngx-quill";
-import {provideRouter} from "@angular/router";
-import {BrowserModule} from "@angular/platform-browser";
-import {GraphQLModule} from "./type/graphql.module";
-import {ProjectModule} from "./pages/private/project/project.module";
-import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import { CookieService } from "ngx-cookie-service";
+import { HttpInterceptorService } from "./services/http.service";
+import { provideToastr, ToastrModule } from "ngx-toastr";
+import { MarkdownModule } from "ngx-markdown";
+import { QuillModule } from "ngx-quill";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { ServiceWorkerModule } from '@angular/service-worker';
 
+import { GraphQLModule } from "./type/graphql.module";
+import { ProjectModule } from "./pages/private/project/project.module";
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
-    imports: [
-      CommonModule,
-      AppRoutingModule,
-      ProjectModule,
-      BrowserAnimationsModule,
-      NgbModule,
-      HttpClientModule,
-      GraphQLModule,
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule,
+    ProjectModule,
+    NgbModule,
+    GraphQLModule,
 
-        ToastrModule.forRoot({
-            positionClass: 'custom-toast-position',
-            preventDuplicates: true,
-            timeOut: 10000,
-            closeButton: true,
-            progressBar: true
-        }),
-        MarkdownModule.forRoot(),
-        QuillModule.forRoot({
-            modules: {
-                toolbar: [
-                    ['bold', 'italic', 'underline'], // Outils de style
-                    [{header: [1, 2, 3, false]}], // En-têtes
-                    [{list: 'ordered'}, {list: 'bullet'}], // Listes
-                    ['link', 'image'], // Liens et images
-                ],
-            },
-        }),
-      BrowserModule,
-      HttpClientModule,
-      GraphQLModule,
-      ServiceWorkerModule.register('ngsw-worker.js', {
-        enabled: !isDevMode(),
-        // Register the ServiceWorker as soon as the application is stable
-        // or after 30 seconds (whichever comes first).
-        registrationStrategy: 'registerWhenStable:30000'
-      }),
+    ToastrModule.forRoot({
+      positionClass: 'custom-toast-position',
+      preventDuplicates: true,
+      timeOut: 10000,
+      closeButton: true,
+      progressBar: true
+    }),
+    MarkdownModule.forRoot(),
+    QuillModule.forRoot({
+      modules: {
+        toolbar: [
+          ['bold', 'italic', 'underline'],
+          [{ header: [1, 2, 3, false] }],
+          [{ list: 'ordered' }, { list: 'bullet' }],
+          ['link', 'image'],
+        ],
+      },
+    }),
 
-
-    ],
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+  ],
 
   bootstrap: [AppComponent],
-  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
-  providers :[
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  providers: [
     CookieService,
     {
       provide: HTTP_INTERCEPTORS,
       useClass: HttpInterceptorService,
       multi: true
     },
-    provideAnimations(), // required animations providers
-    provideToastr(), // Toastr providers
+    provideAnimations(),
+    provideToastr(),
     provideNativeDateAdapter(),
     provideRouter(appRoutes),
   ],
-
-
-
 })
 export class AppModule { }
